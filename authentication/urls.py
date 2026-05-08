4ca67c195df8234848812d42d2cf67a24a6c73eb
Will <mabelane.w@gmail.com>
2026-05-08 19:05:03 +0200

    Set up Django template frontend and login page

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, LoginView, MeView, AdminOnlyView, MemberOnlyView
+from .views import login_page, logout_page
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('me/', MeView.as_view(), name='me'),
     path('admin-only/', AdminOnlyView.as_view(), name='admin_only'),
     path('member-only/', MemberOnlyView.as_view(), name='member_only'),
+    path('login-page/', login_page, name='login_page'),
+    path('logout/', logout_page, name='logout_page'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 from .permissions import IsAdminRole, IsMemberRole
 from .serializers import RegisterSerializer, MeSerializer, EmailTokenObtainPairSerializer
@@ -40,3 +43,27 @@ class MemberOnlyView(APIView):
 
     def get(self, request):
         return Response({"message": "Welcome, member."})
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        messages.error(request, "Invalid username or password.")
+
+    return render(request, "authentication/login.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("home")
